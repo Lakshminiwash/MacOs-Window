b5dfc0fd493e9c2af46451571d8c3999db29d145
Lakshminiwash <lakshminiwash07rr@gmail.com>
2026-04-09 17:47:42 +0530

    fix image path

--- src/components/Taskbar.jsx
+++ src/components/Taskbar.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
+import githubIcon from "../assets/github.webp"
+import notesIcon from "../assets/notes.webp"
+import pdfIcon from "../assets/pdf.webp"
+import calenderIcon from "../assets/calender.webp"  
+import spotifyIcon from "../assets/spotify.webp"
+import gmailIcon from "../assets/gmail.webp"
+import linkIcon from "../assets/link.webp"
 import "./taskbar.scss"
 
 const Taskbar = ({setWindowsState}) => {
   return (
     <footer className='taskbar'>
         <div className='icon github' onClick={() => { setWindowsState(state => ({ ...state, github: true })) }}>   
-            <img src="/src/assets/github.webp" alt="" />
+            <img src={githubIcon} alt="" /> 
         </div>
         <div className='icon notes' onClick={() => { setWindowsState(state => ({ ...state, note: true })) }}>
-            <img src="/src/assets/notes.webp" alt="" />
+            <img src={notesIcon} alt="" />
         </div>
         <div className='icon pdf' onClick={() => { setWindowsState(state => ({ ...state, resume: true })) }}>
-            <img src="/src/assets/pdf.webp" alt="" />
+            <img src={pdfIcon} alt="" />
         </div>
         <div className='icon calender'  onClick={()=>{window.open("https://calendar.google.com/","_blank")}}>
-            <img src="/src/assets/calender.webp" alt="" />
+            <img src={calenderIcon} alt="" />
         </div>
         <div className='icon spotify' onClick={() => { setWindowsState(state => ({ ...state, spotify: true })) }}>
-            <img src="/src/assets/spotify.webp" alt="" />
+            <img src={spotifyIcon} alt="" />
         </div>
         <div className='icon gmail' onClick={()=>{window.open("mailto:lakshminiwashpatel@example.com","_blank")}}>
-            <img src="/src/assets/gmail.webp" alt="" />
+            <img src={gmailIcon} alt="" />
         </div>
         <div className='icon link' onClick={()=>{window.open("https://www.linkedin.com/in/lakshmi-niwash-patel-212469332/","_blank")}}>
-            <img src="/src/assets/link.webp" alt="" />
+            <img src={linkIcon} alt="" />
         </div>
     </footer> 
   )
